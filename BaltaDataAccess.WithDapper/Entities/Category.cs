b00030ec9cfd215214229526f483be8b35677dc0
carlosalbertofilho <solrac.cassf@hotmail.com>
2024-02-05 22:46:03 -0300

    The most significant changes involve the replacement of the initial "Hello, World!" console application code in `Program.cs` with a new code block that interacts with a SQL Server database. This new code block uses the `System`, `Microsoft.Data.SqlClient`, `Dapper`, and `BaltaDataAccess.WithDapper.Entities` namespaces. A new class `Category` has also been added in a new file `Category.cs` under the namespace `BaltaDataAccess.WithDapper.Entities`.
    List of changes:
    
    1. The initial "Hello, World!" console application code in `Program.cs` has been replaced with a new code block that uses the `System`, `Microsoft.Data.SqlClient`, `Dapper`, and `BaltaDataAccess.WithDapper.Entities` namespaces. This change allows the program to interact with a SQL Server database and perform operations on it.
    
    2. A constant string `connectionString` has been defined, which is a connection string for a SQL Server database. This change allows the program to connect to a specific SQL Server database.
    
    3. A `SqlConnection` object is created using the `connectionString`, and the connection is opened. This change allows the program to establish a connection with the SQL Server database.
    
    4. A SQL query is executed using Dapper's `Query` method to fetch data from the `Category` table in the database. The result is stored in the `categories` variable. This change allows the program to retrieve data from the `Category` table in the database.
    
    5. The program then iterates over the `categories` collection and writes each category's `Id`, `Title`, and `Url` to the console. This change allows the program to display the retrieved data in the console.
    
    6. After iterating over the categories, the database connection is closed and a message indicating this is written to the console. This change ensures that the database connection is properly closed after the operation.
    
    7. A new class `Category` has been added in a new file `Category.cs` under the namespace `BaltaDataAccess.WithDapper.Entities`. This class has three properties: `Id`, `Title`, and `Url`. This change provides a model for the data retrieved from the `Category` table in the database.

---
+++ BaltaDataAccess.WithDapper/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace BaltaDataAccess.WithDapper.Entities
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+    }
+}
--- BaltaDataAccess.WithDapper/Program.cs
+++ BaltaDataAccess.WithDapper/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Microsoft.Data.SqlClient;
+using Dapper;
+using BaltaDataAccess.WithDapper.Entities;
+
+const string connectionString
+    = "Server=10.211.55.2,1433;Database=balta;User Id=sa;Password=1q2w3e4r@#$;Trusted_Connection=False; TrustServerCertificate=True;";
+
+using (var connection = new SqlConnection(connectionString))
+{
+    connection.Open();
+    Console.WriteLine("Conexão aberta!");
+
+    var categories
+        = connection.Query<Category>(
+                "SELECT [Id], [Title], [Url] FROM [Category]"
+            );
+
+    foreach (var category in categories)
+    {
+        Console.WriteLine($"{category.Id} - {category.Title} - {category.Url}");
+    }
+
+    connection.Close();
+    Console.WriteLine("Conexão fechada!");
+}
