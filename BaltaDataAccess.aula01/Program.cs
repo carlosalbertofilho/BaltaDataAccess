d32d64e1932917709f3b4dd95a7c2aeb698a4d21
carlosalbertofilho <solrac.cassf@hotmail.com>
2024-02-05 21:50:08 -0300

    Adicionar arquivos de projeto.

---
+++ BaltaDataAccess.aula01/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Data.SqlClient;
+
+const string connectionString = "Server=10.211.55.2,1433;Database=balta;User Id=sa;Password=1q2w3e4r@#$";
+
+var connection = new SqlConnection(connectionString);
+
+connection.Open();
+
+Console.WriteLine("Conexão aberta!");
+
+connection.Close();
+
+Console.WriteLine(connectionString);
