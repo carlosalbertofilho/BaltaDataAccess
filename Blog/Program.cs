51526961c19d303a82db61b44eff7ca430f39028
carlosalbertofilho <carlosalberto_filho@outlook.com>
2024-02-18 15:13:04 -0300

    #Repositories: UserRepository fix

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -1,13 +1,26 @@
-﻿
-using Blog.Repositories;
+﻿using Blog.Repositories;
+using Microsoft.Data.SqlClient;
 
-UserRepository userRepository = new();
+/*
+         * Docker Test C = 10.211.55.2
+         * "Server=10.211.55.2,1433;Database=Blog;User Id=sa;Password=1q2w3e4r@#$;Trusted_Connection=False;TrustServerCertificate=True;";
+         * Docker Test H 
+         * """Data Source=DESKTOP-4P9S4FF\SQLEXPRESS;Initial Catalog=Blog;Integrated Security=True; TrustServerCertificate=True;""";
+         */
+const string CONNECTION_STRING
+    = """Data Source=DESKTOP-4P9S4FF\SQLEXPRESS;Initial Catalog=Blog;Integrated Security=True; TrustServerCertificate=True;""";
 
-userRepository.ReadUsers();
+SqlConnection connection = new(CONNECTION_STRING);
+
+UserRepository userRepository = new(connection);
+
+userRepository.GetUsers()?.ForEach(user => Console.WriteLine(user));
+
 Console.WriteLine("\n-------------\n");
-userRepository.ReadUser(1);
+Console.WriteLine(userRepository.GetUser(1));
+
 Console.WriteLine("\n-------------\n");
-//userRepository.CreateUser(new User
+//userRepository.Create(new User
 //{
 //    Name = "Equipe Blog"
 //,   Email = "equipe@teste.com"
@@ -18,5 +31,5 @@ Console.WriteLine("\n-------------\n");
 //,   CreatedAt = DateTime.Now
 //});
 //Console.WriteLine("\n-------------\n");
-userRepository.UpdateUser(15, name: "Equipe Blog | Suporte", bio: "Equipe de suporte");
+userRepository.Update(15, name: "Equipe Blog | Suporte", bio: "Equipe de suporte");
 
