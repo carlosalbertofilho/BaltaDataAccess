a20b7c2643b6514df48e8f5874ed4307611c5685
carlosalbertofilho <carlosalberto_filho@outlook.com>
2024-02-18 09:45:46 -0300

    #Dcoker H: troca para docker H

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -1,16 +1,16 @@
 ﻿
 /*
  * Docker Test C = 10.211.55.2
- * "Server=10.211.55.2,1433;Database=Blog;User Id=sa;Password=1q2w3e4r@#$;Trusted_Connection=False; TrustServerCertificate=True;";
+ * "Server=10.211.55.2,1433;Database=Blog;User Id=sa;Password=1q2w3e4r@#$;Trusted_Connection=False;TrustServerCertificate=True;";
  * Docker Test H 
- * "Data Source=DESKTOP-4P9S4FF\\SQLEXPRESS;Initial Catalog=Blog;Integrated Security=True; TrustServerCertificate=True;";
+ * """Data Source=DESKTOP-4P9S4FF\SQLEXPRESS;Initial Catalog=Blog;Integrated Security=True; TrustServerCertificate=True;""";
  */
 using Blog.Models;
 using Dapper.Contrib.Extensions;
 using Microsoft.Data.SqlClient;
 
 const string CONNECTION_STRING
-    = "Server=10.211.55.2,1433;Database=Blog;User Id=sa;Password=1q2w3e4r@#$;Trusted_Connection=False; TrustServerCertificate=True;";
+    = """Data Source=DESKTOP-4P9S4FF\SQLEXPRESS;Initial Catalog=Blog;Integrated Security=True; TrustServerCertificate=True;""";
 
 ReadUsers();
 
